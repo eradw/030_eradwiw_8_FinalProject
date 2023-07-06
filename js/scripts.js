0d476d14ea07668c3e94c24113575c42f15990d6
eradww_Register_Github <eradw64@gmail.com>
2023-07-06 21:35:16 +0700

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+/*!
+* Start Bootstrap - Personal v1.0.1 (https://startbootstrap.com/template-overviews/personal)
+* Copyright 2013-2023 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-personal/blob/master/LICENSE)
+*/
+// This file is intentionally blank
+// Use this file to add JavaScript to your project
+
+// typing text hero
+const typed = new Typed(".typing-text", {
+    strings: ["Era Dwi Wulandari"],
+    loop: true,
+    typeSpeed: 55,
+    backSpeed: 25,
+    backDelay: 500,
+  });
